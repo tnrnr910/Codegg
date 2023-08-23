40367bde04a77189fc0511064abecc8a5feb5eb0
tnrnr910 <wjrgkfus@gmail.com>
2023-08-23 15:50:25 +0900

    Feat: 내 프로필 페이지 기본 구성 완료

--- src/Pages/MyPages/MyProfilePage.tsx
+++ src/Pages/MyPages/MyProfilePage.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React from "react"
 
 function MyProfilePage() {
-  return <div>MyProfilePage</div>;
+  return (
+    <>
+      <div>
+        <div>프로필 관리</div>
+        <div>
+          <div>프사</div>
+          <div>뱃지</div>
+          <div>에그마요</div>
+        </div>
+        <div>
+          <div>
+            <div>팔로워</div>
+            <div>팔로잉</div>
+            <div>작성글</div>
+          </div>
+          <div>보유 포인트</div>
+          <div>이메일</div>
+          <div>
+            <div>관심 있는 기술 태그</div>
+            <div>사용 중인 기술이나 관심있는 기술 태그를 선택해주세요.</div>
+          </div>
+          <div>비밀번호 변경</div>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default MyProfilePage;
+export default MyProfilePage
