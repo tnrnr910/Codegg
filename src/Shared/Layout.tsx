7fe4fa002c5a03cacf62b1dcdf3fb1e49e0122c0
tnrnr910 <wjrgkfus@gmail.com>
2023-08-23 14:58:46 +0900

    Feat: LayOut에 Outlet 및 헤더, 푸터 자리 추가

--- src/Shared/Layout.tsx
+++ src/Shared/Layout.tsx
@@ -1,7 +1,14 @@
 import React from "react"
+import { Outlet } from "react-router"
 
 function Layout() {
-  return <div>Layout</div>
+  return (
+    <>
+      <div>헤더자리</div>
+      <Outlet />
+      <div>푸터자리</div>
+    </>
+  )
 }
 
 export default Layout
