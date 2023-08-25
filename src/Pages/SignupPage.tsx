9d2ca66629521e49e94ea9faa4120a82b6006676
tnrnr910 <wjrgkfus@gmail.com>
2023-08-25 12:51:17 +0900

    Style: 불필요한 세미콜론 제거

--- src/Pages/SignupPage.tsx
+++ src/Pages/SignupPage.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from "react"
 
 function SignupPage() {
-  return <div>SignupPage</div>;
+  return <div>SignupPage</div>
 }
 
-export default SignupPage;
+export default SignupPage
