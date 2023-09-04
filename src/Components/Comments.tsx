b2d554a38f1d3d1e376e8a6ef3f2cdb9b528c6bc
ji1e <code3784@gmail.com>
2023-09-04 13:11:49 +0900

    Feat: 댓글 삭제 기능 추가

--- src/Components/Comments.tsx
+++ src/Components/Comments.tsx
@@ -7,11 +7,19 @@ import "moment/locale/ko"
 import { PiSiren } from "react-icons/pi"
 import { AiOutlineLike } from "react-icons/ai"
 import { FaRegComment } from "react-icons/fa"
-import { addDoc, collection, getDocs, query } from "firebase/firestore"
-import { db, auth } from "../axios/firebase"
 import { useParams } from "react-router-dom"
 import { useQuery } from "react-query"
 import { getPosts } from "../axios/api"
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  query,
+  orderBy
+} from "firebase/firestore"
+import { db, auth } from "../axios/firebase"
 
 interface CommentInterface {
   commentContent: string | undefined
@@ -23,7 +31,7 @@ interface CommentInterface {
   postId: string | undefined
   postUserEmail: string | undefined
   postUserDisplayName: string | undefined
-  id?: any
+  id?: string
 }
 
 function Comments() {
@@ -43,40 +51,37 @@ function Comments() {
   const [commentsData, setCommentsData] = useState<CommentInterface[]>([])
   // const date = new Date(commentsData.commentTime*1000)
 
-  // 댓글 조회
-  useEffect(() => {
-    const fetchComments = async () => {
-      const querySnapshot = await getDocs(query(collection(db, "comments")))
+  // 댓글 가져오는 함수
+  const fetchComments = async () => {
+    const querySnapshot = await getDocs(
+      query(collection(db, "comments"), orderBy("commentTime"))
+    )
 
-      const initialComments: CommentInterface[] = []
+    const initialComments: CommentInterface[] = []
 
-      querySnapshot.forEach((doc) => {
-        // eslint-disable-next-line @typescript-eslint/no-base-to-string, @typescript-eslint/restrict-template-expressions
-        console.log(`1${doc.id} => ${doc.data()}`)
-        if (id !== undefined) {
-          const commentData: CommentInterface = {
-            commentContent: doc.data().commentContent,
-            commentTime: doc.data().commentTime,
-            commentUserEmail: doc.data().commentUserEmail,
-            commentUserProfileImg: doc.data().commentUserProfileImg,
-            commentUserDisplayName: doc.data().commentUserDisplayName,
-            isSecret: doc.data().isSecret,
-            postId: doc.data().postId,
-            postUserEmail: doc.data().postUserEmail,
-            postUserDisplayName: doc.data().postUserDisplayName,
-            id: doc.id
-          }
-          initialComments.push(commentData)
-          setCommentsData([...initialComments])
+    querySnapshot.forEach((doc) => {
+      // eslint-disable-next-line @typescript-eslint/no-base-to-string, @typescript-eslint/restrict-template-expressions
+      console.log(`1${doc.id} => ${doc.data()}`)
+      if (id !== undefined) {
+        const commentData: CommentInterface = {
+          commentContent: doc.data().commentContent,
+          commentTime: doc.data().commentTime,
+          commentUserEmail: doc.data().commentUserEmail,
+          commentUserProfileImg: doc.data().commentUserProfileImg,
+          commentUserDisplayName: doc.data().commentUserDisplayName,
+          isSecret: doc.data().isSecret,
+          postId: doc.data().postId,
+          postUserEmail: doc.data().postUserEmail,
+          postUserDisplayName: doc.data().postUserDisplayName,
+          id: doc.id
         }
-      })
-      console.log(222, commentsData)
-    }
+        initialComments.push(commentData)
+        setCommentsData([...initialComments])
+      }
+    })
+  }
 
-    void fetchComments()
-  }, [])
-
-  // 댓글 인풋값을 inputTextf로 샛함
+  // 댓글 인풋값을 inputText로 샛함
   const onInputChange = (event: { target: { name: any; value: any } }) => {
     const {
       target: { name, value }
@@ -87,12 +92,17 @@ function Comments() {
     console.log(inputText)
   }
 
-  // 댓글 등록 버튼 클릭 시 실행
+  // 댓글 조회
+  useEffect(() => {
+    void fetchComments()
+  }, [])
+
+  // 댓글 등록
   async function addComment(event: any) {
     event.preventDefault()
     const newComment = {
       commentContent: inputText,
-      commentTime: moment().format("L"),
+      commentTime: moment().format("MMMM Do YYYY, h:mm:ss a"),
       commentUserEmail: auth.currentUser?.email,
       commentUserProfileImg: auth.currentUser?.photoURL,
       commentUserDisplayName: auth.currentUser?.displayName,
@@ -102,22 +112,29 @@ function Comments() {
       postUserDisplayName: postData?.postDisplayName
     }
 
-    console.log(newComment)
-    setCommentsData((prev) => [...prev, newComment]) // 콜백함수로 기존 데이터에 추가하기 > ui렌더링 변경 > db요청(addDoc)
     setInputText("")
 
     await addDoc(collection(db, "comments"), newComment) // await가 batching처리 방해
-    // console.log("Document written with ID: ", docRef.id)
+    await fetchComments()
+  }
+
+  // 댓글 삭제
+  const deleteComment = async (id: string) => {
+    await deleteDoc(doc(db, "comments", id))
+
+    setCommentsData((prev) => {
+      return prev.filter((element) => element.id !== id)
+    })
   }
 
   return (
     <CommentContainer>
       <CommentHead>댓글</CommentHead>
       <CommentLists>
+        {/* 댓글 영역 */}
         {commentsData
           .filter((item) => item.postId === id?.toString())
           .map((commentItem) => {
-            console.log(commentItem)
             return (
               <React.Fragment key={commentItem.id}>
                 <Comment>
@@ -134,27 +151,50 @@ function Comments() {
                     </CommentContents>
                   </CommentLeft>
                   <CommentRight>
-                    <CommentEdit>
-                      <button>수정</button>
-                      <button>삭제</button>
-                    </CommentEdit>
-                    <CommentButtons>
-                      <button>
-                        <PiSiren size="20px" />
-                        신고
-                      </button>
-                      <button>
-                        <AiOutlineLike size="20px" />
-                        좋아요
-                      </button>
-                      <button>
-                        <FaRegComment size="18px" />
-                        답글
-                      </button>
-                    </CommentButtons>
+                    {auth.currentUser?.email ===
+                    commentItem?.commentUserEmail ? (
+                      <CommentEdit>
+                        <button>수정</button>
+                        <button
+                          // eslint-disable-next-line @typescript-eslint/no-misused-promises
+                          onClick={async () => {
+                            if (commentItem.id !== undefined) {
+                              await deleteComment(commentItem.id)
+                            }
+                          }}
+                        >
+                          삭제
+                        </button>
+                      </CommentEdit>
+                    ) : null}
+                    {auth.currentUser?.email ===
+                    commentItem?.commentUserEmail ? (
+                      <CommentButtons>
+                        <button>
+                          <FaRegComment size="18px" />
+                          답글
+                        </button>
+                      </CommentButtons>
+                    ) : (
+                      <CommentButtons>
+                        <button>
+                          <PiSiren size="20px" />
+                          신고
+                        </button>
+                        <button>
+                          <AiOutlineLike size="20px" />
+                          좋아요
+                        </button>
+                        <button>
+                          <FaRegComment size="18px" />
+                          답글
+                        </button>
+                      </CommentButtons>
+                    )}
                   </CommentRight>
                 </Comment>
-                {/* <ReplyComment>
+                {/* 대댓글 영역
+                <ReplyComment>
                   <CommentLeft style={{ width: "55%", marginLeft: "1rem" }}>
                     <PiArrowBendDownRightBold
                       style={{ color: "#787878" }}
@@ -244,6 +284,7 @@ const Comment = styled.div`
   padding: 1rem 0;
 `
 
+// 대댓글 영역
 // const ReplyComment = styled.div`
 //   display: flex;
 //   justify-content: space-between;
@@ -294,6 +335,7 @@ const CommentEdit = styled.div`
   & > button {
     all: unset;
     padding: 0 4px;
+    cursor: pointer;
   }
 
   & > button:first-child {
