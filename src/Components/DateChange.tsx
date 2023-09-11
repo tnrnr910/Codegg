f48e41ae7d64210c5d5a186496c695b79d46bb54
tnrnr910 <wjrgkfus@gmail.com>
2023-09-11 21:52:08 +0900

    Feat: 쪽지 발송 기능 및 받은 쪽지 보이기 구현 / QnA, 검색결과, 코딩 팁, 모임, 좋아요 한 글, 내가 쓴 글의 timestamp 가져오는 방식 변경

--- src/Components/DateChange.tsx
+++ src/Components/DateChange.tsx
@@ -1,9 +1,9 @@
 export function formatDate(timestamp: number): string {
-  const date = new Date(timestamp * 1000)
+  const date = new Date(timestamp)
 
   const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
+  const month = String(date.getMonth() + 1).padStart(2, "0")
+  const day = String(date.getDate()).padStart(2, "0")
 
   const formattedDate = `${year}.${month}.${day}`
 
