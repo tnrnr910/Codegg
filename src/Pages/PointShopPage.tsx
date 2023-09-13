b53d2b8938eafe22f6e2c57cad989dcbc29303ee
jinheepark <jhp202@naver.com>
2023-09-13 11:36:01 +0900

    Feat: 포인트샵 진행중

--- src/Pages/PointShopPage.tsx
+++ src/Pages/PointShopPage.tsx
@@ -6,15 +6,31 @@ interface ShopItem {
   id: number
   name: string
   price: number
+
+  style?: {
+    color?: string
+    fontWeight?: string
+  }
 }
 
 const PointShopPage: React.FC = () => {
   const [userPoints, setUserPoints] = useState(2000)
-  const [selectedItem, setSelectedItem] = useState<ShopItem | null>(null)
+  const [selectedItems, setSelectedItems] = useState<ShopItem[]>([])
+  const [showDescription, setShowDescription] = useState<boolean>(false)
 
   const items: ShopItem[] = [
-    { id: 1, name: "게시글 타이틀 볼드체 변경", price: 100 },
-    { id: 2, name: "게시글 타이틀 색상 변경 - 주황", price: 300 },
+    {
+      id: 1,
+      name: "게시글 타이틀 볼드체 변경",
+      price: 100,
+      style: { fontWeight: "bold" }
+    },
+    {
+      id: 2,
+      name: "게시글 타이틀 색상 변경 - 주황",
+      price: 300,
+      style: { color: "orange" }
+    },
     { id: 3, name: "게시글 타이틀 색상 변경 - 빨강", price: 300 },
     { id: 4, name: "게시글 타이틀 사이즈 변경 - 15pt", price: 200 },
     { id: 5, name: "게시글 타이틀 색상 변경 - 초록", price: 100 },
@@ -24,41 +40,68 @@ const PointShopPage: React.FC = () => {
 
   const handleItemSelect = (item: ShopItem) => {
     if (userPoints >= item.price) {
-      setSelectedItem(item)
-      setUserPoints(userPoints - item.price)
+      if (selectedItems.includes(item)) {
+        setSelectedItems(selectedItems.filter((i) => i !== item))
+      } else {
+        setSelectedItems([...selectedItems, item])
+      }
     } else {
       alert("포인트가 부족합니다.")
     }
   }
 
-  // useEffect(() => {
-  //   // 여기에 아이템을 구매했을 때 타이틀 효과를 적용하는 로직을 추가할 수 있습니다.
-  //   // selectedItem에 선택한 아이템이 저장되어 있으므로, 이를 활용하여 타이틀 효과를 적용하세요.
-  // }, [])
+  const toggleDescription = () => {
+    setShowDescription(!showDescription)
+  }
+
+  const applySelectedItems = () => {
+    const totalPrice = selectedItems.reduce(
+      (total, item) => total + item.price,
+      0
+    )
+    if (userPoints >= totalPrice) {
+      setUserPoints(userPoints - totalPrice)
+      setSelectedItems([])
+      alert("선택한 아이템이 적용되었습니다.")
+    } else {
+      alert("포인트가 부족합니다.")
+    }
+  }
 
   return (
     <PointShopWrap>
       <PointShopMenuBar activeMenuItem={""} />
       <ShopContainer>
-        <h1>포인트샵</h1>
+        <StyledTitle>포인트샵</StyledTitle>
         <p>보유 포인트: {userPoints}</p>
 
         <ItemList>
           {items.map((item) => (
-            <Item key={item.id}>
-              <ItemName>{item.name}</ItemName>
-              <ItemPrice>{item.price} 포인트</ItemPrice>
-              <BuyButton
-                onClick={() => {
+            <Item
+              key={item.id}
+              onMouseEnter={toggleDescription}
+              onMouseLeave={toggleDescription}
+            >
+              <CheckBox
+                type="checkbox"
+                checked={selectedItems.includes(item)}
+                onChange={() => {
                   handleItemSelect(item)
                 }}
-                disabled={selectedItem === item}
-              >
-                {selectedItem === item ? "선택됨" : "구매"}
-              </BuyButton>
+              />
+              <ItemName>{item.name}</ItemName>
+              <ItemPrice>{item.price} 포인트</ItemPrice>
+
+              {showDescription && (
+                <DescriptionContainer>
+                  {item.name}에 대한 설명입니다.
+                </DescriptionContainer>
+              )}
             </Item>
           ))}
         </ItemList>
+
+        <ApplyButton onClick={applySelectedItems}>적용하기</ApplyButton>
       </ShopContainer>
     </PointShopWrap>
   )
@@ -106,18 +149,36 @@ const ItemPrice = styled.span`
   flex: 1;
 `
 
-const BuyButton = styled.button`
-  background-color: #007bff;
+const CheckBox = styled.input`
+  flex: 0.5;
+  height: 20px;
+  width: 20px;
+`
+
+const ApplyButton = styled.button`
+  background-color: #0c356a;
   color: #fff;
   border: none;
   border-radius: 4px;
-  padding: 8px 16px;
+  padding: 8px 8px;
   cursor: pointer;
+  margin-top: 20px;
+  width: 100px;
+  margin-left: auto;
+`
 
-  &:disabled {
-    background-color: #ccc;
-    cursor: not-allowed;
-  }
+const StyledTitle = styled.div`
+  margin-bottom: 3.125rem;
+  font-size: 1.5625rem;
+  font-weight: bold;
+`
+
+const DescriptionContainer = styled.div`
+  background-color: #f9f9f9;
+  padding: 10px;
+  border: 1px solid #ddd;
+  margin-top: 10px;
+  display: none;
 `
 
 export default PointShopPage
