27309b8c245fb9c5f20444f9a4a8097cabfa4947
tnrnr910 <wjrgkfus@gmail.com>
2023-09-21 21:59:41 +0900

    Feat: 에러페이지 추가 및 UI 완성

--- src/Pages/ErrorPage.tsx
+++ src/Pages/ErrorPage.tsx
@@ -1,7 +1,36 @@
 import React from "react"
+import { styled } from "styled-components"
+import { useNavigate } from "react-router"
 
 function ErrorPage() {
-  return <div>ErrorPage</div>
+  const navigate = useNavigate()
+
+  return (
+    <>
+      <ErrorImg src={require("../asset/img/errorImg.png")} alt="badgeImage" />
+      <ErrorMent>This page could not be found.</ErrorMent>
+      <ErrorLink
+        onClick={() => {
+          navigate("/")
+        }}
+      >
+        메인페이지로 이동하기
+      </ErrorLink>
+    </>
+  )
 }
 
 export default ErrorPage
+
+const ErrorImg = styled.img``
+
+const ErrorMent = styled.div`
+  font-size: 40px;
+  font-weight: bold;
+  margin-bottom: 10px;
+  margin-top: 30px;
+`
+const ErrorLink = styled.div`
+  font-weight: bold;
+  cursor: pointer;
+`
